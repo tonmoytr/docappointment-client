b84469f1ab9fe3db81deaa799b81fbad843ee265
tonmoytr <tonmoytr0110@gmail.com>
2026-05-22 21:22:07 +0600

    chore: remove unnecessary comment and add spacing for code clarity

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,11 @@ import TopRatedDoctors from "@/components/Home/TopRatedDoctors";
 import { heroSlides } from "@/config/heroData";
 import { infoItems } from "@/config/infoItems";
 
+//
+
 export default function HomePage() {
   return (
     <div className="pb-20 space-y-20">
-      {/* Hero Banner Section Container Block */}
       <section className="">
         <HeroSlider slides={heroSlides} />
         <InfoRibbon infoItems={infoItems} />
